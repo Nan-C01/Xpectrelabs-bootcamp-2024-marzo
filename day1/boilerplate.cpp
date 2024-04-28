7f1e3489352ab84d64dd1950fe3c24e9cddbf1d1
Nan-C01 <nancycastellonsuarezcat@hotmail.com>
2024-04-28 15:24:34 -0600

    añadí una forma de salir del programa para que no se cierre de inmediato al ejecutar el .exe

--- day1/boilerplate.cpp
+++ day1/boilerplate.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main() {
-    
+    char salida; // esta variable se utiliza al final del programa para cerrarlo. Es como "presiona cualquier tecla para continuar", pero hay que escribir algo y luego presionar Enter, así que es un poco más tardado.
+
+    cout << "Escribe un caracter y presiona Enter para salir.";
+    cin >> salida;
     return 0;
 }
